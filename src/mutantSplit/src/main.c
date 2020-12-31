93ab85c48383600212397f4996ad6b09bd26c95b
Gueren Drive <gdrive@ga-c1.msk.21-school.ru>
2020-12-31 23:01:37 +0300

    Counting word len is started. Need more tests !

--- src/mutantSplit/src/main.c
+++ src/mutantSplit/src/main.c
@@ -14,7 +14,18 @@
 #include <fcntl.h>
 #include "mutant_split.h"
 
-int	main(void)
+static void	print_arr_len(char **strings)
+{
+	size_t	len = 0;
+	while (*strings != NULL)
+	{
+		len += 1;
+		strings += 1;
+	}
+	printf("user len    = %zu\n", len);
+}
+
+int			main(void)
 {
 	/*
 	** Tests
@@ -29,46 +40,67 @@ int	main(void)
 	printf("~~~ basic ~~~\n");
 	c = ';';
 	char	*s1 = "123";
-	char	*s2 = "123;456";
-	char	*s3 = "123;456;789";
-	char	*s4 = "123;456;";
-	char	*s5 = ";123;456;789";
-	char	*s6 = ";123;456;";
-	char	*s7 = ";123;456;789;";
+	char	*s2 = "123;4567";
+	char	*s3 = "123;4567;789123";
+	char	*s4 = "12;4567;";
+	char	*s5 = ";1;456;789123456";
+	char	*s6 = ";1234567;4;";
+	char	*s7 = ";1234567;456789;7891;";
 
 	printf("input: |%s|\n", s1);
 	printf("correct len = %d\n", 1);
 	res = mutant_split(s1, c);
+	//print_arr_len(res);
+	printf("WORDS   len = %d\n", 3);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s2);
 	printf("correct len = %d\n", 2);
 	res = mutant_split(s2, c);
+	//print_arr_len(res);
+	printf("WORDS   len = %d, %d\n", 3, 4);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s3);
 	printf("correct len = %d\n", 3);
 	res = mutant_split(s3, c);
+	//print_arr_len(res);
+	printf("WORDS   len = %d, %d, %d\n", 3, 4, 6);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s4);
 	printf("correct len = %d\n", 2);
 	res = mutant_split(s4, c);
+	//print_arr_len(res);
+	printf("WORDS   len = %d, %d\n", 2, 4);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s5);
 	printf("correct len = %d\n", 3);
 	res = mutant_split(s5, c);
+	//print_arr_len(res);
+	printf("WORDS   len = %d, %d, %d\n", 1, 3, 9);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s6);
 	printf("correct len = %d\n", 2);
 	res = mutant_split(s6, c);
+	//print_arr_len(res);
+	printf("WORDS   len = %d, %d\n", 7, 1);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s7);
 	printf("correct len = %d\n", 3);
 	res = mutant_split(s7, c);
+	//print_arr_len(res);
+	printf("WORDS   len = %d, %d, %d\n", 7, 6, 4);
+	free(res);
 	printf("\n");
 
 	printf("~~~~~~~~~~~~~\n\n");
@@ -76,53 +108,67 @@ int	main(void)
 	//Hard
 	printf("~~~ hard ~~~~\n");
 	char	*s8 = ";;;;;123;;;;";
-	char	*s9 = "123;;;;;;456";
-	char	*s10 = "123;;;;;456;;;;;789";
-	char	*s11 = "123;;;;;456;;;;";
-	char	*s12 = ";;;;123;;;;456;;;;789";
-	char	*s13 = ";;;;123;;;;;456;;;";
-	char	*s14 = ";;;;123;;;;456;789;;;;";
+	char	*s9 = "12;;;;;;456";
+	char	*s10 = "1;;;;;46;;;;;789";
+	char	*s11 = "123;;;;;456789;;;;";
+	char	*s12 = ";;;;12345;;;;45;;;;7";
+	char	*s13 = ";;;;12345;;;;;456789;;;";
+	char	*s14 = ";;;;1234;;;;456789;7891234567;;;;";
 
 	printf("input: |%s|\n", s8);
 	printf("correct len = %d\n", 1);
 	res = mutant_split(s8, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s9);
 	printf("correct len = %d\n", 2);
 	res = mutant_split(s9, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s10);
 	printf("correct len = %d\n", 3);
 	res = mutant_split(s10, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s11);
 	printf("correct len = %d\n", 2);
 	res = mutant_split(s11, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s12);
 	printf("correct len = %d\n", 3);
 	res = mutant_split(s12, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s13);
 	printf("correct len = %d\n", 2);
 	res = mutant_split(s13, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s14);
 	printf("correct len = %d\n", 3);
 	res = mutant_split(s14, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("~~~~~~~~~~~~~\n\n");
 
 	//BasicWithProtecting
 	printf("~~~ Basic With Protecting ~~~\n");
-	s1 = "123\\;456"; //1
+	s1 = "1\\;46"; //1
 	s2 = "12'3;4'56"; //1
 	s3 = "\"123;456\";789"; //2
 	s4 = "123\\\\;456;"; //2
@@ -133,36 +179,50 @@ int	main(void)
 	printf("input: |%s|\n", s1);
 	printf("correct len = %d\n", 1);
 	res = mutant_split(s1, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s2);
 	printf("correct len = %d\n", 1);
 	res = mutant_split(s2, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s3);
 	printf("correct len = %d\n", 2);
 	res = mutant_split(s3, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s4);
 	printf("correct len = %d\n", 2);
 	res = mutant_split(s4, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s5);
 	printf("correct len = %d\n", 3);
 	res = mutant_split(s5, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s6);
 	printf("correct len = %d\n", 1);
 	res = mutant_split(s6, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s7);
 	printf("correct len = %d\n", 3);
 	res = mutant_split(s7, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
@@ -180,36 +240,50 @@ int	main(void)
 	printf("input: |%s|\n", s8);
 	printf("correct len = %d\n", 1);
 	res = mutant_split(s8, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s9);
 	printf("correct len = %d\n", 2);
 	res = mutant_split(s9, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s10);
 	printf("correct len = %d\n", 3);
 	res = mutant_split(s10, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s11);
 	printf("correct len = %d\n", 2);
 	res = mutant_split(s11, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s12);
 	printf("correct len = %d\n", 3);
 	res = mutant_split(s12, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s13);
 	printf("correct len = %d\n", 4);
 	res = mutant_split(s13, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 	printf("input: |%s|\n", s14);
 	printf("correct len = %d\n", 3);
 	res = mutant_split(s14, c);
+	//print_arr_len(res);
+	free(res);
 	printf("\n");
 
 
--- src/mutantSplit/src/mutant_split.c
+++ src/mutantSplit/src/mutant_split.c
@@ -26,34 +26,110 @@ static void		init_str(t_split_str *str, char *s, char c)
 
 }
 
+/*
+** From count_elements.c
+ */
+
+int			is_new_word(t_split_str *s);
+int			is_end_or_sep(t_split_str *s);
+
+/////////////////////////
+
+size_t			count_word_len(t_split_str s)
+{
+	size_t	len;
+
+	len = 0;
+	while (is_end_or_sep(&s))
+	{
+		if (s.current != s.after_protecting)
+		{
+			len = s.after_protecting - s.current;
+			s.current = s.after_protecting;
+		}
+		else if (*s.current != '\0')
+		{
+			len++;
+			s.current += 1;
+		}
+		s.after_protecting = skip_protected(s.current);
+	}
+	return (len);
+}
+
+void				skip_sep(t_split_str *s)
+{
+	while (*s->current == s->separator && *s->current != '\0')
+	{
+		s->current += 1;
+	}
+	s->after_protecting = skip_protected(s->current);
+}
+
+/*
+** Unnecessary.
+*/
+
+static void		skip_word(t_split_str *s)
+{
+	while (is_end_or_sep(s))
+	{
+		if (s->current != s->after_protecting)
+		{
+			s->current = s->after_protecting;
+		}
+		else if (*s->current != '\0')
+		{
+			s->current += 1;
+		}
+		s->after_protecting = skip_protected(s->current);
+	}
+	if (*s->current != '\0')
+		s->current += 1;
+	s->after_protecting = skip_protected(s->current);
+}
+
+void			copy_and_skip_word(char *dst, t_split_str *s)
+{
+	size_t	word_len;
+
+	//printf("*s->current: |%c|\n", *s->current);
+	word_len = count_word_len(*s);
+	skip_word(s);
+	printf("word    len = %zu\n", word_len);
+
+}
+
 char			**mutant_split(const char *s, char c)
 {
+	t_arr_strings	strings;
 	char	**result;
 	size_t	len;
 	size_t	i;
 	t_split_str	str;
 
 	init_str(&str, (char*)s, c);
-	len = count_elements(str);
-	printf("------MyLen = %zu\n", len);
+	strings.len = count_elements(str);
+	printf("------MyLen = %zu\n", strings.len);
 
-	result = (char**)malloc(sizeof(char*) * (len + 1));
-	if (result == NULL)
+	strings.arr = (char**)malloc(sizeof(char*) * (strings.len + 1));
+	if (strings.arr == NULL)
 		return (NULL);
-	result[len] = NULL;
+	strings.arr[strings.len] = NULL;
+
+
 	i = 0;
-	while (i < len)
+	while (i < strings.len)
 	{
-		//result[i] = copy_command(s, c);
-		//s = skip_command;
+		skip_sep(&str);
+		copy_and_skip_word(strings.arr[i], &str);
 		i += 1;
 	}
 
 
 
 
-	free(result);
 	//free_string_arr(result);
-	return (NULL);
+	return (strings.arr);
 }
 
