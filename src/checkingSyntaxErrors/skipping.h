b97a4f2bf03024510908dd46cac285bf01306e50
unknovwn <dmitri.smirnoov@gmail.com>
2021-01-12 23:12:00 +0300

    ,,,

--- src/checkingSyntaxErrors/skipping.h
+++ src/checkingSyntaxErrors/skipping.h
@@ -1,19 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_error_and_exit.c                             :+:      :+:    :+:   */
+/*   skipping.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgeneviv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gdrive <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/12 16:44:59 by mgeneviv          #+#    #+#             */
-/*   Updated: 2021/01/12 23:11:18 by mgeneviv         ###   ########.fr       */
+/*   Created: 2021/01/11 18:36:53 by gdrive            #+#    #+#             */
+/*   Updated: 2021/01/11 18:36:54 by gdrive           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#ifndef SKIPPING_H
+# define SKIPPING_H
 
-void	print_error_and_exit(void)
-{
-	ft_fprintf(STDERR, "%s: %s\n", SHELL_NAME, strerror(errno));
-	exit(1);
-}
+char	*skip_protected(char *current);
+int		is_protect(char c);
+char	*skip_symbols(char *current, char c);
+char	*skip_bslash(char *current);
+char	*skip_space(char *current);
+
+#endif
--- src/checkingSyntaxErrors/utils.h
+++ src/checkingSyntaxErrors/utils.h
@@ -1,19 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_error_and_exit.c                             :+:      :+:    :+:   */
+/*   semicolon_and_pipe.h                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgeneviv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gdrive <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/12 16:44:59 by mgeneviv          #+#    #+#             */
-/*   Updated: 2021/01/12 23:11:18 by mgeneviv         ###   ########.fr       */
+/*   Created: 2021/01/11 18:55:38 by gdrive            #+#    #+#             */
+/*   Updated: 2021/01/11 18:55:39 by gdrive           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#ifndef UTILS_H
+# define UTILS_H
 
-void	print_error_and_exit(void)
-{
-	ft_fprintf(STDERR, "%s: %s\n", SHELL_NAME, strerror(errno));
-	exit(1);
-}
+int		is_semicolon(char c);
+int		is_pipe(char c);
+int		is_redirect(char c);
+
+char	*check_error_near(char *current);
+char	*check_error_near_with_redirect_to(char *current);
+
+#endif
