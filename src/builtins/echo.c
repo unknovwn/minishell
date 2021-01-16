bfee790c40165152e142a24cc54a29f003cabaee
unknovwn <dmitri.smirnoov@gmail.com>
2021-01-16 22:36:31 +0300

    Fix echo

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: mgeneviv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 13:16:49 by mgeneviv          #+#    #+#             */
-/*   Updated: 2021/01/16 16:52:31 by mgeneviv         ###   ########.fr       */
+/*   Updated: 2021/01/16 22:25:07 by mgeneviv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,17 @@
 int		echo_command(int argc, char **argv)
 {
 	int	i;
+	int	newline;
 
-	i = 1;
+	newline = !(argc > 1 && (ft_strcmp(argv[1], "-n") == 0));
+	i = 1 + !newline;
 	while (i < argc)
 	{
 		if (i > 1)
 			ft_putstr(" ", STDOUT);
 		ft_putstr(argv[i++], STDOUT);
 	}
-	if (!(argc > 1 && (ft_strcmp(argv[1], "-n") == 0)))
+	if (newline)
 		ft_putstr("\n", STDOUT);
 	return (0);
 }
