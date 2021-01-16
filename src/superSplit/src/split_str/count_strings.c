054b7b096a8b08e7248bb99d9360b8a06cae5a39
unknovwn <dmitri.smirnoov@gmail.com>
2021-01-16 21:38:54 +0300

    Fix SUPERsplit

--- src/superSplit/src/split_str/count_strings.c
+++ src/superSplit/src/split_str/count_strings.c
@@ -1,16 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   count_result_len.c                                 :+:      :+:    :+:   */
+/*   count_strings.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gdrive <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/29 15:24:19 by gdrive            #+#    #+#             */
-/*   Updated: 2020/12/29 15:24:23 by gdrive           ###   ########.fr       */
+/*   Updated: 2021/01/16 21:34:58 by mgeneviv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdbool.h>
 
 #include "split_str.h"
 
@@ -21,7 +19,7 @@ size_t		count_strings(t_split_str s)
 	word_count = 0;
 	while (*s.current != '\0')
 	{
-		if (is_sep(&s) == false)
+		if (!(is_sep(&s)))
 		{
 			word_count += 1;
 			skip_string(&s);
--- src/superSplit/src/split_str/skip_sep.c
+++ src/superSplit/src/split_str/skip_sep.c
@@ -6,11 +6,9 @@
 /*   By: gdrive <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/02 02:52:35 by gdrive            #+#    #+#             */
-/*   Updated: 2021/01/02 02:52:36 by gdrive           ###   ########.fr       */
+/*   Updated: 2021/01/16 21:35:03 by mgeneviv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdbool.h>
 
 #include "split_str.h"
 
@@ -21,7 +19,7 @@ int			is_sep(t_split_str *s)
 
 static int	is_sep_and_is_not_end(t_split_str *s)
 {
-	return (is_sep(s) == true && *s->current != '\0');
+	return (is_sep(s) && *s->current != '\0');
 }
 
 void		skip_sep(t_split_str *s)
